a7f6e70aa46d54cbace42fe6672505c64eaa6f21
linhnguyen0702 <linhyang0702@gmail.com>
2025-11-16 20:25:48 +0700

    add env

--- admin/src/config/index.js
+++ admin/src/config/index.js
@@ -12,11 +12,11 @@ const config = {
   },
   production: {
     API_BASE_URL:
-      import.meta.env.VITE_API_BASE_URL || "https://your-api-domain.com",
+      import.meta.env.VITE_BACKEND_URL || "https://your-api-domain.com",
     CLIENT_BASE_URL:
       import.meta.env.VITE_CLIENT_BASE_URL || "https://orebiclient.reactbd.com",
     ADMIN_BASE_URL:
-      import.meta.env.VITE_ADMIN_BASE_URL || "https://orebiadmin.reactbd.com",
+      import.meta.env.VITE_BACKEND_URL || "https://orebiadmin.reactbd.com",
     NODE_ENV: "production",
     DEBUG: false,
     LOG_LEVEL: "error",
